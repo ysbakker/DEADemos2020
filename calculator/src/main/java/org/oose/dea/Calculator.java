b49054ff1690ab16996b24776155978c089cf707
Yorrick Bakker <y.bakker@me.com>
2020-02-05 10:29:48 +0100

    Add 'substract', 'times' and 'divide' methods and tests

--- calculator/src/main/java/org/oose/dea/Calculator.java
+++ calculator/src/main/java/org/oose/dea/Calculator.java
@@ -4,4 +4,16 @@ public class Calculator {
     public int add(int value1, int value2) {
         return value1 + value2;
     }
+
+    public int substract(int value1, int value2) {
+        return value1 - value2;
+    }
+
+    public int times(int value1, int value2) {
+        return value1 * value2;
+    }
+
+    public int divide(int value1, int value2) {
+        return value1 / value2;
+    }
 }
--- calculator/src/test/java/org/oose/dea/CalculatorTest.java
+++ calculator/src/test/java/org/oose/dea/CalculatorTest.java
@@ -11,4 +11,25 @@ public class CalculatorTest {
         assertEquals(4,calculator.add(2,2));
         assertEquals(100,calculator.add(25,75));
     }
+
+    @Test
+    public void testSubstract() {
+        Calculator c = new Calculator();
+        assertEquals(100, c.substract(105,5));
+        assertEquals(10, c.substract(200,190));
+    }
+
+    @Test
+    public void testTimes() {
+        Calculator c = new Calculator();
+        assertEquals(100, c.times(10,10));
+        assertEquals(69, c.times(3,23));
+    }
+
+    @Test
+    public void testDivide() {
+        Calculator c = new Calculator();
+        assertEquals(1, c.divide(100,100));
+        assertEquals(20, c.divide(120,6));
+    }
 }
